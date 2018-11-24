b05e8b37f8f4ce22a8a6f33b555533b41230552b
Andy_KANG <30693282+AndyKANG98@users.noreply.github.com>
2018-11-24 19:17:42 +0800

    Add weekly_event_count feature

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Drop-MOOC-Prediction
-COMP4331 Data Mining Group Project
+>  COMP4331 Data Mining Group Project
+
+
+
+* 1st round: event_count
+
+```
+             precision    recall  f1-score   support
+
+          0       0.70      0.51      0.59      4902
+          1       0.88      0.94      0.91     19111
+
+avg / total       0.84      0.85      0.85     24013
+```
+
+<br>
+
+* 2nd round: add weekly_event_count
+
+```
+ precision    recall  f1-score   support
+
+          0       0.74      0.57      0.64      4902
+          1       0.90      0.95      0.92     19111
+
+avg / total       0.86      0.87      0.86     24013
+```
--- preprocess.py
+++ preprocess.py
@@ -17,15 +17,80 @@ class Preprocessor():
         self.enroll = Enrollment(enroll_path)
         self.truth = Truth(truth_path)
 
+    def get_truth(self):
+        return self.truth
+    
+    def get_merge_df(self):
+        enroll_df = self.enroll.get_data()
+        log_df = self.log.get_data()
+
+        course_date_df = pd.DataFrame.from_dict(self.course_date.get_course_info()).T
+        course_date_df.reset_index(inplace = True)
+        course_date_df.rename(columns={"index": "course_id"}, inplace = True)
+
+        enroll_course_df = enroll_df.merge(course_date_df, on='course_id', how='inner')
+        log_enroll_course_df = log_df.merge(enroll_course_df, on = 'enrollment_id', how='inner')
+
+        return log_enroll_course_df
+
+    def weekly_event_count(self):
+        start_time = time.time()
+
+        merge_df = self.get_merge_df().filter(items=['enrollment_id', 'time', 'event', 'start'])
+        merge_df= merge_df.assign(week_index = merge_df['time'].values.astype('datetime64[W]') - merge_df['start'].values.astype('datetime64[W]'))
+        weekly_event_count = merge_df.groupby(['enrollment_id','week_index']).event.count().unstack(fill_value=0)
+        weekly_event_count = weekly_event_count.add_suffix("_count").rename(columns={'0 days 00:00:00_count': 'week_one_count', '7 days 00:00:00_count': 'week_two_count', 
+        '14 days 00:00:00_count': 'week_three_count', '21 days 00:00:00_count': 'week_four_count', '28 days 00:00:00_count': 'week_five_count', '35 days 00:00:00_count': 'week_six_count'})
+        weekly_event_count.reset_index(inplace=True)
+
+        print("weekly_event_count features extracted! %f seconds taken" % (time.time()-start_time))
+        print("Shape of the weekly_event_count dataframe: ", weekly_event_count.shape)
+        return weekly_event_count
+        
     def event_count(self):
         start_time = time.time()
         
         log_df = self.log.get_data()
         event_count_df = log_df.groupby(['enrollment_id']).event.value_counts().unstack(fill_value=0)
         event_count_df = event_count_df.add_suffix("_count")
-        event_count_df.reset_index(level=0, inplace=True)
+        event_count_df.reset_index(inplace=True)
         
         print("event_count features extracted! %f seconds taken" % (time.time()-start_time))
-        print("Shape of the dataframe: ", event_count_df.shape)
+        print("Shape of the event_count dataframe: ", event_count_df.shape)
         
         return event_count_df
+    
+    def get_features_all(self):
+        event_count_df = self.event_count()
+        # weekly_event_count = self.weekly_event_count()
+        # features_df = pd.merge(event_count_df, weekly_event_count, how='inner', on='enrollment_id')
+        return event_count_df
+    
+    def get_values_all(self):
+        features_df = self.get_features_all()
+        df_all = features_df.merge(self.truth.get_data(), left_on='enrollment_id', right_on='enrollment_id', how='inner')
+        df_all_shuffled = df_all.sample(frac=1)
+        
+        X = df_all_shuffled.drop(columns=['label', 'enrollment_id']).values
+        y = df_all_shuffled['label'].values
+        
+        print("Getting raw values for X, y done!")
+        print("The shape of X: %s; shape of y: %s" % (X.shape, y.shape))
+        return X, y
+
+    def get_values_partial(self, ratio):
+        features_df = self.get_features_all()
+        df_all = features_df.merge(self.truth.get_data(), left_on='enrollment_id', right_on='enrollment_id', how='inner')
+        df_1 = df_all[(df_all['label']==1)].sample(frac=ratio)
+        df_partial = pd.concat([df_1, df_all[(df_all['label']==0)]])
+        df_partial_shuffled = df_partial.sample(frac=1)
+
+        X = df_partial_shuffled.drop(columns=['label', 'enrollment_id']).values
+        y = df_partial_shuffled['label'].values
+
+        print("Getting partial X, y done!")
+        print("The shape of X: %s; shape of y: %s" % (X.shape, y.shape))
+        y_ratio = np.count_nonzero(y)/len(y)
+        print("The ratio of 1 in labels: ", "{:.2%}".format(y_ratio))
+
+        return X, y
